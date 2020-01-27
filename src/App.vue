5a7f10bbf92ef1003031da8086e0652f07c0d69a
Inna <shpota_inna@ukr.net>
2020-01-27 17:16:03 +0200

    The Vue Instance

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Product product="Socks" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Product from "./components/Product.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Product
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Product.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ product }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 4vh 0 0;
+}
+</style>

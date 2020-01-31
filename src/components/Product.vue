db1266ba04736e86888f460cd50c0467c70890fa
Inna <shpota_inna@ukr.net>
2020-01-31 09:35:41 +0200

    Computed Properties

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -5,10 +5,10 @@
     </div>
 
     <div class="product-info">
-      <h1>{{ product }}</h1>
+      <h1>{{ title }}</h1>
       <p v-if="inStock">In Stock</p>
       <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
-      <span v-if="onSale">On Sale!</span>
+      <p>{{ sale }}</p>
 
       <ul>
         <li v-for="detail in details" :key="detail">{{ detail }}</li>
@@ -18,19 +18,24 @@
         <li v-for="size in sizes" :key="size">{{ size }}</li>
       </ul>
 
-      <div v-for="variant in variants" 
-           :key="variant.variantId"
-           class="color-box"
-           :style="{backgroundColor: variant.variantColor}"
-           @mouseover="updateProduct(variant.variantImage)">
-      </div>
+      <div
+        v-for="(variant, index) in variants"
+        :key="variant.variantId"
+        class="color-box"
+        :style="{ backgroundColor: variant.variantColor }"
+        @mouseover="updateProduct(index)"
+      ></div>
 
-      <button v-on:click="addToCart" 
-              :disabled="!inStock"
-              :class="{ disabledButton: !inStock }">
-            Add to cart
-        </button>
-      <button @click="removeFromCart">Remove from cart</button>
+      <button
+        v-on:click="addToCart"
+        :disabled="!inStock"
+        :class="{ disabledButton: !inStock }"
+      >Add to cart</button>
+      <button 
+        @click="removeFromCart"
+        :disabled="cart <= 0"
+        :class="{ disabledButton: cart <= 0 }"
+      >Remove from cart</button>
 
       <div class="cart">
         <p>Cart({{ cart }})</p>
@@ -47,23 +52,24 @@ export default {
   },
   data() {
     return {
-      image:
-        "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-      inStock: true,
-      onSale: false,
+      onSale: true,
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       details: ["80% cotton", "20% polyester", "Gender-neutral"],
       variants: [
         {
           variantId: 2234,
           variantColor: "green",
           variantImage:
-            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+          variantQuantity: 10
         },
         {
           variantId: 2235,
           variantColor: "blue",
           variantImage:
-            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0
         }
       ],
       sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
@@ -74,11 +80,28 @@ export default {
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
     },
     removeFromCart() {
       this.cart -= 1;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " are on sale!";
+      }
+      return this.brand + " " + this.product + " are not on sale";
     }
   }
 };
